f722cd138d3ba302525f4b9c4826fa61f8097dd7
avanish7390 <singhavanish7700@gmail.com>
2024-03-22 17:36:23 +0530

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react'
 import {Link} from 'react-router-dom'
-import Logo from '../images/logo.png'
+import Logo from '../images/logos.png'
 import {FaBars} from "react-icons/fa"
 import {AiOutlineClose} from "react-icons/ai"
 
